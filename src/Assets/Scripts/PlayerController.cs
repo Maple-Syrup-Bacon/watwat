6de2614dcf38764d4c22f6f54782a65b3d0f6d62
Hilmar Tryggvason <hilmart@gmail.com>
2018-02-28 13:45:34 +0000

    Fiddling with ProBuilder

---
+++ src/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField]
+    private float speed = 10f;
+
+    private Rigidbody body;
+    private Vector2 movement;
+
+    // Use this for initialization
+    private void Start()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        movement = Vector2.zero;
+        movement.x = Input.GetAxis("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        if (movement.x != 0.0f)
+        {
+            body.velocity = new Vector3(movement.x * speed * Time.fixedDeltaTime, body.velocity.y, 0.0f);
+            Debug.Log(body.velocity);
+        }
+        else
+        {
+            body.velocity = new Vector3(0.0f, body.velocity.y, 0.0f);
+        }
+    }
+}

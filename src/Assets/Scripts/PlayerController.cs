47a1f1de89bbdba6b4777c641ae6b69c8f242711
Hilmar Tryggvason <hilmart@gmail.com>
2018-03-01 10:21:21 +0000

    Added basic stuff, also messing around

--- src/Assets/Scripts/PlayerController.cs
+++ src/Assets/Scripts/PlayerController.cs
@@ -1,39 +1,70 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Rewired;
 
 public class PlayerController : MonoBehaviour
 {
 
     [SerializeField]
-    private float speed = 10f;
+    private float movementSpeed = 1000f;
+    [SerializeField]
+    private float jumpForce = 10f;
+
+    private Rewired.Player player;
 
     private Rigidbody body;
-    private Vector2 movement;
+    private float movement;
+    private bool jump;
+    private bool grounded;
 
     // Use this for initialization
     private void Start()
     {
+        player = ReInput.players.GetPlayer(0);
         body = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     private void Update()
     {
-        movement = Vector2.zero;
-        movement.x = Input.GetAxis("Horizontal");
+        movement = 0.0f;
+
+        if (player.GetButton("Move Left"))
+        {
+            movement += -1.0f;
+        }
+        if (player.GetButton("Move Right"))
+        {
+            movement += 1.0f;
+        }
+
+        if (!jump && grounded)
+        {
+            jump = player.GetButton("Jump");
+        }
     }
 
     private void FixedUpdate()
     {
-        if (movement.x != 0.0f)
+        if (movement != 0.0f)
         {
-            body.velocity = new Vector3(movement.x * speed * Time.fixedDeltaTime, body.velocity.y, 0.0f);
-            Debug.Log(body.velocity);
+            body.velocity = new Vector3(movement * movementSpeed * Time.fixedDeltaTime, body.velocity.y, 0.0f);
         }
         else
         {
             body.velocity = new Vector3(0.0f, body.velocity.y, 0.0f);
         }
+
+        if (jump)
+        {
+            jump = false;
+            body.AddForce(Vector3.up * jumpForce * Time.fixedDeltaTime, ForceMode.Impulse);
+        }
+    }
+
+    public void SetGrounded(bool grounded)
+    {
+        this.grounded = grounded;
     }
 }

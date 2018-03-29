9ab6361f62286a58d8c845b4282cf458a12e6bb8
Birkir Brynjarsson <birkir@Birkirs-MacBook-Air-2.local>
2018-03-29 12:06:21 +0000

    Add smoke particles to asteroids

--- src/Assets/Scripts/ObjectMover.cs
+++ src/Assets/Scripts/ObjectMover.cs
@@ -6,20 +6,15 @@ public class ObjectMover : MonoBehaviour {
 
 	public float minSpeed;
 	public float maxSpeed;
-	public float minRotation;
-	public float maxRotation;
 	private Vector3 direction;
-	private float rotationSpeed;
 	private float movementSpeed;
 
 	void Start () {
 		movementSpeed = Random.Range(minSpeed, maxSpeed);
-		rotationSpeed = Random.Range(minRotation, maxRotation);
 		direction = new Vector3(Random.Range(-1f,1f), Random.Range(-1f,1f), 0);
 	}
 	
 	void Update () {
-		transform.Rotate(new Vector3(0.0f, 0.0f, rotationSpeed * Time.fixedDeltaTime));
 		transform.position += direction * movementSpeed * Time.fixedDeltaTime;
 	}
 }
---
+++ src/Assets/Scripts/ObjectRotator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectRotator : MonoBehaviour {
+
+	public float minRotation;
+	public float maxRotation;
+	private float rotationSpeed;
+	void Start () {
+		rotationSpeed = Random.Range(minRotation, maxRotation);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(new Vector3(0.0f, 0.0f, rotationSpeed * Time.fixedDeltaTime));
+	}
+}
